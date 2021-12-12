0d95547f434b01db5850e47d1048ecaf3c7e28dc
Joao Freitas <medeirosjoaoadv@gmail.com>
2021-12-12 12:18:57 -0300

    criacao projeto crash firebase

--- 05_crashlytics/lib/screens/transaction_form.dart
+++ 05_crashlytics/lib/screens/transaction_form.dart
@@ -1,4 +1,4 @@
-import 'package:comunicacao_http/components/transaction_auth_dialog.dart';
+import 'package:crashlytics/components/transaction_auth_dialog.dart'; 
 import 'package:flutter/material.dart';
 import '../models/contact.dart';
 import '../models/transaction.dart';
